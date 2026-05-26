e053b6c67e42c7240c96eca9b1ea959eb79545bd
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-05-26 10:11:32 +0530

    Time: 1 ms (31.51%) | Memory: 9.2 MB (83.32%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    string removeDuplicateLetters(string s) {
+    string removeDuplicateLetters(string s){
         int n = s.length();
-        string result;
         vector<bool> taken(26,false);
         vector<int> lastIndex(26);
+        string result;
         for (int i=0;i<n;i++){
             char ch = s[i];
             lastIndex[ch-'a'] = i;
@@ -13,7 +13,7 @@ public:
             char ch = s[i];
             int idx = ch-'a';
             if (taken[idx]==true) continue;
-            while (result.length()>0 && result.back() > ch && lastIndex[result.back()-'a']>i){
+            while (result.length()>0 && result.back()>ch && lastIndex[result.back()-'a']>i){
                 taken[result.back()-'a'] = false;
                 result.pop_back();
             }
