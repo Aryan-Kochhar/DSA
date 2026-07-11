3644011cec0c2993eabfacc1934499b9db968a00
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-07-11 23:12:37 +0530

    Time: 63 ms (43.61%) | Memory: 44.1 MB (18.43%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,28 +9,28 @@
  */
 class Solution {
 public:
-    bool getPath(TreeNode *root,vector<TreeNode*> &ans,TreeNode *n){
-        if (!root) return false;
-        ans.push_back(root);
-        if (root == n) return true;
-        if (getPath(root->left,ans,n) || getPath(root->right,ans,n)) return true;
-        ans.pop_back();
-        return false;
-    }
+    // bool getPath(TreeNode *root,vector<TreeNode*> &ans,TreeNode *n){
+    //     if (!root) return false;
+    //     ans.push_back(root);
+    //     if (root == n) return true;
+    //     if (getPath(root->left,ans,n) || getPath(root->right,ans,n)) return true;
+    //     ans.pop_back();
+    //     return false;
+    // }
 
-    vector<TreeNode*> rootToNode(TreeNode *root,TreeNode *n){
-        vector<TreeNode*> ans;
-        getPath(root,ans,n);
-        return ans;
-    }
+    // vector<TreeNode*> rootToNode(TreeNode *root,TreeNode *n){
+    //     vector<TreeNode*> ans;
+    //     getPath(root,ans,n);
+    //     return ans;
+    // }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == nullptr) return nullptr;
-        vector<TreeNode*> path1 = rootToNode(root,p);
-        vector<TreeNode*> path2 = rootToNode(root,q);
+        if (root == nullptr || root == p || root == q) return root;
+        TreeNode *left = lowestCommonAncestor(root->left,p,q);
+        TreeNode *right = lowestCommonAncestor(root->right,p,q);
 
-        int i = 0;
-        while (i<path1.size() && i<path2.size() && path1[i]==path2[i]) i++;
-        return path1[i-1];
+        if (left == NULL) return right;
+        else if (right == NULL) return left;
+        else return root;
     }
 };
