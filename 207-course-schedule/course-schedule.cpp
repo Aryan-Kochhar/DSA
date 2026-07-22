d950e577ab2046ed8b2fc54790083d2723c91b68
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-07-22 18:56:32 +0530

    Time: 7 ms (45.16%) | Memory: 20 MB (32.68%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,26 +3,26 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         vector<int> indegree(numCourses,0);
-        for (auto &it:prerequisites){
+        for (auto it: prerequisites){
             adj[it[1]].push_back(it[0]);
             indegree[it[0]]++;
         }
+        int cnt = 0;
         queue<int> q;
         for (int i=0;i<numCourses;i++){
             if (indegree[i]==0){
                 q.push(i);
             }
         }
-        int cnt = 0;
         while (!q.empty()){
             int node = q.front();
             q.pop();
             cnt++;
-            for (auto it: adj[node]){
+            for (auto it:adj[node]){
                 indegree[it]--;
-                if (indegree[it] == 0) q.push(it);
+                if (indegree[it]==0) q.push(it);
             }
         }
-        return cnt == numCourses;
+        return cnt==numCourses;
     }
 };
