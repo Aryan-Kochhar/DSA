896da4bca51166e3dab67da142cedc6e2ad41cc8
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-07-27 11:20:04 +0530

    Time: 2 ms (30.82%) | Memory: 20.7 MB (94.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    void setColZeroes(vector<vector<int>> &matrix,int i,int j,int n,int m){
-        for (int i=0;i<n;i++){
-            if (matrix[i][j]!=0) matrix[i][j] = 69420;
-        }
-    }
-    void setRowsZeroes(vector<vector<int>> &matrix,int i,int j,int n,int m){
-        for (int j=0;j<m;j++){
-            if (matrix[i][j]!=0) matrix[i][j] = 69420;
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        vector<int> row(n,0);
+        vector<int> col(m,0);
         for (int i=0;i<n;i++){
             for (int j=0;j<m;j++){
                 if (matrix[i][j]==0){
-                    setRowsZeroes(matrix,i,j,n,m);
-                    setColZeroes(matrix,i,j,n,m);
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 69420)
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (row[i] || col[j]){
                     matrix[i][j] = 0;
+                }
             }
         }
+        
     }
 };
