d4643e2460a363bbdd675f0552cf673c1c25e7e3
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-08-05 13:44:04 +0530

    Time: 1 ms (83.28%) | Memory: 19.3 MB (68.01%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
+    bool dfsCheck(int src,const vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathVis){
+        vis[src] = 1;
+        pathVis[src] = 1;
+        for (auto it:adj[src]){
+            if (!vis[it]){
+                if (dfsCheck(it,adj,vis,pathVis)==true) return true;
+            }
+            else if (pathVis[it]) return true;
+        }
+        pathVis[src] = 0;
+        return false;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> vis(numCourses,0);
+        vector<int> pathVis(numCourses,0);
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses,0);
-        for (auto it: prerequisites){
+
+        for (auto &it : prerequisites) {
             adj[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
         }
-        int cnt = 0;
-        queue<int> q;
+
         for (int i=0;i<numCourses;i++){
-            if (indegree[i]==0){
-                q.push(i);
+            if (!vis[i]){
+                if (dfsCheck(i,adj,vis,pathVis)) return false;
             }
-        }
-        while (!q.empty()){
-            int node = q.front();
-            q.pop();
-            cnt++;
-            for (auto it:adj[node]){
-                indegree[it]--;
-                if (indegree[it]==0) q.push(it);
-            }
-        }
-        return cnt==numCourses;
+        }    
+        return true;
     }
 };
