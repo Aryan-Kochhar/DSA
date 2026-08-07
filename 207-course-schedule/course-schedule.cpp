45f49da30f87e4d57f4a69e71ca121252a0820db
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-08-08 00:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.85%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    bool dfsCheck(int src,const vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathVis){
-        vis[src] = 1;
-        pathVis[src] = 1;
-        for (auto it:adj[src]){
+    bool detectCycle(int node,const vector<vector<int>> &adj,vector<int> &vis,vector<int> &pathVis){
+        vis[node] = 1;
+        pathVis[node] = 1;
+        for (auto it:adj[node]){
             if (!vis[it]){
-                if (dfsCheck(it,adj,vis,pathVis)==true) return true;
+                if (detectCycle(it,adj,vis,pathVis) == true) return true;
             }
-            else if (pathVis[it]) return true;
+            else if (pathVis[it] == 1) return true;
         }
-        pathVis[src] = 0;
+        pathVis[node] = 0;
         return false;
     }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses,0);
-        vector<int> pathVis(numCourses,0);
         vector<vector<int>> adj(numCourses);
-
-        for (auto &it : prerequisites) {
+        for (auto &it:prerequisites){
             adj[it[1]].push_back(it[0]);
         }
 
+        vector<int> vis(numCourses,0);
+        vector<int> pathVis(numCourses,0);
         for (int i=0;i<numCourses;i++){
             if (!vis[i]){
-                if (dfsCheck(i,adj,vis,pathVis)) return false;
+                if (detectCycle(i,adj,vis,pathVis)==true) return false;
             }
-        }    
+        }
         return true;
     }
 };
