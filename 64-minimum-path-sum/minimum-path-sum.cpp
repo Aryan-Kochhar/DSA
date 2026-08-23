f9dc86ab8932b7358bdaae96cbf1ef01ec39b9d3
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-08-24 00:34:32 +0530

    Time: 4 ms (15.25%) | Memory: 16.5 MB (16.54%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int wowza(int i,int j,vector<vector<int>> &dp,const vector<vector<int>> &grid){
-        if (i==0 && j==0) return grid[i][j];
-        if (i<0 || j<0) return 1e9;
-        if (dp[i][j]!=-1) return dp[i][j];
-        int up = grid[i][j] + wowza(i-1,j,dp,grid);
-        int left = grid[i][j] + wowza(i,j-1,dp,grid);
-        return dp[i][j] = min(up,left);
+    int wowza(int n,int m,vector<vector<int>> &dp,const vector<vector<int>> &grid){
+        if (m==0 && n==0) return grid[0][0];
+        //if (i<0 || j<0) return 1e9;
+        //if (dp[i][j]!=-1) return dp[i][j];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int up = 1e9,left=1e9;
+                if (i==0 && j==0){ dp[i][j] = grid[i][j];continue;}
+                if (i>0) up = grid[i][j] + dp[i-1][j];
+                if (j>0) left = grid[i][j] + dp[i][j-1];
+                dp[i][j] = min(up,left);
+            }
+        }
+        return dp[n-1][m-1];
     }
 
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return wowza(n-1,m-1,dp,grid);
+        return wowza(n,m,dp,grid);
     }
 };
