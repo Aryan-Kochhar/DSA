e77ccca34bacb3138d77c17d9bbf402a4d885e5f
Aryan Kochhar <aryankochhar2005@gmail.com>
2026-09-06 12:46:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,15 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
-using namespace std;
-
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n1 = s.length();
-        int n2 = t.length();
+        int n1 = s.size();
+        int n2 = t.size();
         if (n1!=n2) return false;
         unordered_map<char,int> mpp;
-        for (char ch:s){
+        for (char ch: s){
             mpp[ch]++;
         }
         for (char ch:t){
-            if (mpp.find(ch)==mpp.end() || mpp[ch]==0){
-                return false;
-            }
+            if (mpp.find(ch)==mpp.end() || mpp[ch]==0) return false;
             mpp[ch]--;
         }
         return true;
